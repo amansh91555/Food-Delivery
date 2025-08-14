2c4a229e50675fb64d1636783d08b451105c5235
Aman Sharma <amansharma993144@gmail.com>
2025-08-14 18:41:10 +0530

    Remove .env file from tracking

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,9 @@
 import orderModel from "../models/orderModels.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe"
+import dotenv from dotenv
+
+dotenv.config();
 
 const stripe= new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user orddr for frontend
